f6cb39e599cf7278acb76970c15b8204e81b8a6b
Your Name <you@example.com>
2026-01-08 12:56:40 +0000

    initial commit

---
+++ crm/crm/doctype/lead_management/lead_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, crm and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Lead Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ crm/crm/doctype/quotation/quotation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, crm and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Quotation(Document):
+	pass
